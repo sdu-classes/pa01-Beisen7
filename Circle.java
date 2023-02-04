71136ec03b97518035394752c8f47b291734b84c
Beisen <113160775+Beisen7@users.noreply.github.com>
2023-02-04 12:42:14 +0600

    Create Circle.java

---
+++ Circle.java
@@ -0,0 +1,15 @@
+public class Circle {
+    double radius;
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+}
